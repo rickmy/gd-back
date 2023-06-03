9f39c79f4b678eedf82a22a85d02c3b0e8f7318c
rickmy <ricky081297@hotmail.com>
2023-06-02 21:24:06 -0500

    ADD auth modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
 import config from './core/config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ load: [config], isGlobal: true }),
     PrismaModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@nestjs/common';
+import { CreateAuthDto } from './dto/create-auth.dto';
+import { UpdateAuthDto } from './dto/update-auth.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class AuthService {
+  constructor(private _prismaService: PrismaService) { }
+  create(createAuthDto: CreateAuthDto) {
+    return this._prismaService.user.findMany();
+  }
+
+  findAll() {
+    return this._prismaService.rol.findMany();
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} auth`;
+  }
+
+  update(id: number, updateAuthDto: UpdateAuthDto) {
+    return `This action updates a #${id} auth`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} auth`;
+  }
+}

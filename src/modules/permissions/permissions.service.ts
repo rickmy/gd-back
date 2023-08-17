7ddb676fcea0c92117fba41f3c24c4185136e414
Ricardo Yaguachi <ricky081297@hotmail.com>
2023-08-16 23:27:11 -0500

    fixed

--- src/modules/permissions/permissions.service.ts
+++ src/modules/permissions/permissions.service.ts
@@ -23,7 +23,14 @@ export class PermissionsService {
   }
 
   async findAll(): Promise<PermissionEntity[]> {
-    return await this._prismaService.permission.findMany();
+    return await this._prismaService.permission.findMany({
+      where: {
+        state: true,
+      },
+      orderBy: {
+        createdAt: 'desc',
+      },
+    });
   }
 
   async findOne(id: string): Promise<PermissionEntity> {
@@ -39,40 +46,6 @@ export class PermissionsService {
       );
     return permission;
   }
-
-  /* async changePassword(data: {
-    userId: number;
-    currentPassword: string;
-    newPassword: string;
-  }): Promise<HttpException> {
-    const user = await this._userService.findByID(data.userId);
-    if (!user)
-      throw new HttpException('Usuario no existe', HttpStatus.BAD_REQUEST);
-    if (!user.state)
-      throw new HttpException(
-        'El usuario se encuentra inactivo/bloqueado',
-        HttpStatus.CONFLICT,
-      );
-    const isMatch = await this._userService.comparePassword(
-      data.currentPassword,
-      user.password,
-    );
-    if (!isMatch)
-      throw new HttpException(
-        'Credenciales invalidas',
-        HttpStatus.UNAUTHORIZED,
-      );
-    const newPassword = await this._userService.hashPassword(data.newPassword);
-    user.password = newPassword;
-    const ok = await this._userService.update(user.id, user);
-    if (!ok)
-      throw new HttpException(
-        'Error al actualizar la contraseña',
-        HttpStatus.UNPROCESSABLE_ENTITY,
-      );
-    return new HttpException('Contraseña actualizada', HttpStatus.OK);
-  }
- */
 
   async update(
     id: string,
